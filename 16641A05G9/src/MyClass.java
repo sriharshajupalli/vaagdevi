da66ddde83ca943015f5eeb73d4a07f7be80c743
Sriharsha <Sriharsha@DESKTOP-D9H8EIF>
2018-12-31 11:41:56 +0530

    commit

---
+++ 16641A05G9/src/MyClass.java
@@ -0,0 +1,13 @@
+
+public class MyClass {
+int MyAccountDetails;
+
+public int getMyAccountDetails() {
+	return MyAccountDetails;
+}
+
+public void setMyAccountDetails(int myAccountDetails) {
+	MyAccountDetails = myAccountDetails;
+}
+
+}
---
+++ 16641A05G9/src/MyClassDriver.java
@@ -0,0 +1,9 @@
+
+public class MyClassDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("16641A05G9");
+	}
+
+}
